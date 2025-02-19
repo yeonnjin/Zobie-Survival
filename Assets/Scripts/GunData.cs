a65f11eabbebf4c4ef2eaeef1a824ba21bac4a1c
yeonjin <xxyeon17@gmail.com>
2025-02-19 17:51:40 +0900

    [ 10일차 ] 250215 : ~684p
    - 인터페이스 : 느슨한 커플링
    - 파티클
    - 스크립터블 오브젝트 : 데이터를 유니티 에셋 형태로 저장(Gun Data)
    - 코루틴 : IEnumerator타입 반환, 대기 시간을 가짐(yield return new WaitForSeconds(시간)), 다른 코드는 정상 실행됨
    - 라인렌더러 : 레이캐스트(Physics.Raycast())
    - IK(<-> FK) : OnAnimatorIK(GetIKHintPosition, SetIKPositionWeight, SetIKPosition, SetIKRotation, ..)

--- Assets/Scripts/GunData.cs
+++ Assets/Scripts/GunData.cs
@@ -1,15 +1,18 @@
 using UnityEngine;
 
-public class GunData
+// 특성 : 어떤 클래스나 매서드, 변수 등에 대한 추가 정보를 제공하는 C# 문법
+// 특성 이름을 대괄호([])로 묶고, 필요에 따라 추가 값을 전달
+[CreateAssetMenu(menuName = "Scriptable/GunData", fileName = "Gun Data")]
+public class GunData : ScriptableObject
 {
-    public AudioClip shotClip; // 발사 소리
-    public AudioClip reloadClip; // 재장전 소리
+    public AudioClip shotClip;          // 발사 소리
+    public AudioClip reloadClip;        // 재장전 소리
 
-    public float damage = 25; // 공격력
+    public float damage = 25;           // 공격력
 
-    public int startAmmoRemain = 100; // 처음에 주어질 전체 탄약
-    public int magCapacity = 25; // 탄창 용량
+    public int startAmmoRemain = 100;   // 처음에 주어질 전체 탄약
+    public int magCapacity = 25;        // 탄창 용량
 
-    public float timeBetFire = 0.12f; // 총알 발사 간격
-    public float reloadTime = 1.8f; // 재장전 소요 시간
+    public float timeBetFire = 0.12f;   // 총알 발사 간격
+    public float reloadTime = 1.8f;     // 재장전 소요 시간
 }
